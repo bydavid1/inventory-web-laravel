4e03b870a8657381574168d1c79981e0bc79c029
byronjd <byronjimenez9911@gmail.com>
2020-05-17 14:15:21 -0600

    Invoice window added
    Error: reset form all not working

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -88,6 +88,66 @@ class SaleController extends Controller
         }
             return back()->with('mensaje', 'Ocurrió un error al registrar la información');
     }
+
+
+      /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function save(Request $request)
+    {            
+        //invoice headers info
+        $sale = new Sales;
+        $sale->name = $request->name;
+        $sale->quantity = $request->grandquantityvalue;
+        $sale->total = $request->grandtotalvalue;
+        $sale->is_deleted = 0;
+
+        if ($sale->save()) {
+        $id = $sale->id;
+        $invoice = ['name' => $sale->name, 'quantity' => $sale->quantity, 'total' => $sale->total, 'date' => $sale->created_at];
+        $invoice_products = array();
+        $counter = $request->trCount;
+        for ($i=1; $i <= $counter; $i++) { 
+            $saleitem = new Sales_item;
+            //database and request handlers
+            $data = ['pnamevalue', 'pcodevalue', 'quantityvalue', 'pricevalue', 'totalvalue'];
+            $db = ['product_name', 'product_code', 'quantity', 'price', 'total'];
+            for ($j=0; $j < 5; $j++) { 
+                //Packing item data to -> $saleitem
+                $modifier = $data[$j] ."". $i;
+                $dbmodifier = $db[$j];
+                $saleitem->$dbmodifier = $request->$modifier;
+            }
+            $saleitem->sale_id = $id;
+            if ($saleitem->save()) {
+            //Update quantity 
+            $product = Products::select('quantity', 'id')->where('code', $saleitem->product_code)->first();
+            $product->quantity = $product->quantity - $saleitem->quantity;
+            $product->save();
+            //Adding to Kardex
+            $kardex = new Kardex;
+            $kardex->tag = "Ingreso por producto";
+            $kardex->tag_code = "IN";
+            $kardex->id_product = $product->id;
+            $kardex->quantity = $saleitem->quantity;
+            $kardex->value = "+" . $saleitem->total;
+            $kardex->unit_price = $saleitem->price;
+            $kardex->invoice_id = $id;
+            $kardex->save();
+            //Adding $saleitems to -> $invoice_products array
+            array_push($invoice_products, $saleitem);
+            }else{
+                $sale->destroy();
+                return response()->json(['message'=>'No se terminó de crear la factura']);
+                }
+            }
+            return response()->json(['message'=>'Factura guardada', 'data' => compact(['invoice', 'invoice_products'])]);
+        }
+        return response()->json(['message'=>'Ocurrió un error al registrar la información']);
+    }
     /**
      * Display the specified resource.
      *
--- resources/views/sales/addSale.blade.php
+++ resources/views/sales/addSale.blade.php
@@ -39,11 +39,12 @@
     </div>
     <!--/Body-->
     <div class="card-body">
-        <form class="form-horizontal" method="POST" action="{{ route('makeInvoice') }}" id="createOrderForm">
+        <form class="form-horizontal" id="createOrderForm">
            @csrf
             <div class="row">
                 <!--/form-group-->
                 <div class="form-group col-lg-6">
+                    <input type="hidden" name="csrf_token" value="{{ csrf_token() }}">
                     <label for="clientName" class="col-sm-4 control-label">Nombre del cliente</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" name="name" placeholder="Cliente"
@@ -120,8 +121,7 @@
             $('#items').DataTable({
                 "serverSide": true,
                 "ajax": "{{ url('api/products/order') }}",
-                "columns": [
-                    {
+                "columns": [{
                         data: 'photo'
                     },
                     {
@@ -137,11 +137,11 @@
                         data: 'custom_quantity'
                     },
                     {
-                        "searchable" : false,
+                        "searchable": false,
                         data: 'name_prov'
                     },
                     {
-                        "searchable" : false,
+                        "searchable": false,
                         data: 'name_categ'
                     },
                     {
@@ -153,5 +153,176 @@
     </script>
     
     @include('product-order.script')
+
+    <script>
+        $('#createOrderForm').unbind('submit').bind('submit', function (stay) {
+            stay.preventDefault();
+            var formdata = $(this).serialize();
+            var url = "{{ route('save') }}";
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: formdata,
+                beforeSend: function () {
+                    //Loader
+                },
+                success: function (response) {
+                    Swal.fire({
+                        position: 'top-end',
+                        type: 'success',
+                        title: response.message,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+
+                    print(response.data);
+                },
+                error: function(){
+                    Swal.fire({
+                        position: 'top-end',
+                        type: 'error',
+                        title: 'error interno',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            });
+        });
+
+        function print(data) {
+            var invoice = data.invoice;
+            var invoice_products = data.invoice_products;
+            var body = "";
+             $.each(invoice_products, function(i, value){
+              body += `<tr><td>` + value.product_code + `</td><td>` + value.product_name + `</td><td>` + value.quantity + `</td><td>$` + value.price + `</td><td>$` + value.total + `</td></tr>`;
+            });
+            var html = `<div class="wrapper">
+                <!-- Main content -->
+                <section class="invoice">
+                    <!-- title row -->
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 class="page-header">
+                                <i class="fas fa-globe"></i> AdminLTE, Inc.
+                                <small class="float-right">Date: ` + invoice['date'] + `</small>
+                            </h2>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- info row -->
+                    <div class="row invoice-info">
+                        <div class="col-sm-4 invoice-col">
+                            From
+                            <address>
+                                <strong>Admin, Inc.</strong><br>
+                                795 Folsom Ave, Suite 600<br>
+                                San Francisco, CA 94107<br>
+                                Phone: (804) 123-5432<br>
+                                Email: info@almasaeedstudio.com
+                            </address>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-4 invoice-col">
+                            To
+                            <address>
+                                <strong>` + invoice['name'] + `</strong><br>
+                                795 Folsom Ave, Suite 600<br>
+                                San Francisco, CA 94107<br>
+                                Phone: (555) 539-1037<br>
+                                Email: john.doe@example.com
+                            </address>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-4 invoice-col">
+                            <b>Invoice #007612</b><br>
+                            <br>
+                            <b>Order ID:</b> 4F3S8J<br>
+                            <b>Payment Due:</b> 2/22/2014<br>
+                            <b>Account:</b> 968-34567
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                    <!-- Table row -->
+                    <div class="row">
+                        <div class="col-12 table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Codigo</th>
+                                        <th>Producto</th>
+                                        <th>Cant</th>
+                                        <th>Precio</th>
+                                        <th>Subtotal</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    ` + body + `
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <!-- accepted payments column -->
+                        <div class="col-6">
+                            <p class="lead">Payment Methods:</p>
+                            <img src="../../dist/img/credit/visa.png" alt="Visa">
+                            <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
+                            <img src="../../dist/img/credit/american-express.png" alt="American Express">
+                            <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+                            <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
+                                Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya
+                                handango imeem plugg dopplr
+                                jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+                            </p>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-6">
+                            <p class="lead">Amount Due 2/22/2014</p>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tr>
+                                        <th style="width:50%">Subtotal:</th>
+                                        <td>` + invoice['total'] + `</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Tax (9.3%)</th>
+                                        <td>$10.34</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Cantidad:</th>
+                                        <td>` + invoice['quantity'] + `</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Total:</th>
+                                        <td>` + invoice['total'] + `</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- ./wrapper -->`;
+
+            var target = window.open('', 'PRINT', 'height=1000,width=1000');
+            target.document.write('<html><head><title>Imprimir</title>');
+            target.document.write('<link rel="stylesheet" href="{{ asset("css/adminlte.min.css") }}">');
+            target.document.write(html);
+            target.document.write('</body></html>');
+            target.document.close();
+            target.focus();
+            target.onload = function () {
+                target.print();
+                target.close();
+                $('#createOrderForm')[0].reset();
+            };
+            }
+    </script>
 
 @endsection
