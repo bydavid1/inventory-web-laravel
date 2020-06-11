22fc148a7aa77bd573abb8e09f73d8caf33c4081
Byron Jimenez <byronjimenez9911@gmail.com>
2020-06-11 10:41:16 -0600

    credits migrations

---
+++ database/migrations/2020_06_11_163555_create_credits_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCreditsItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credits_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('credit_id');
+            $table->foreign('credit_id')->references('id')->on('credits')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->float('discount');
+            $table->float('unit_price');
+            $table->float('unit_tax');
+            $table->float('total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credits_items');
+    }
+}
