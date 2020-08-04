7e1069226074ac07ec824ee7ca88f82be3903d2f
Byron Jimenez <byronjimenez9911@gmail.com>
2020-08-03 19:38:30 -0600

    Update 2020_06_26_225743_create_prices_table.php

--- database/migrations/2020_06_26_225743_create_prices_table.php
+++ database/migrations/2020_06_26_225743_create_prices_table.php
@@ -18,11 +18,11 @@ class CreatePricesTable extends Migration
             $table->timestamps();
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->float('price');
+            $table->float('price')->nullable()->default('0.00');
             $table->unsignedBigInteger('tax_id');
             $table->foreign('tax_id')->references('id')->on('tax_rules')->onDelete('cascade');
-            $table->float('price_incl_tax');
-            $table->float('utility');
+            $table->float('price_incl_tax')->nullable()->default('0.00');
+            $table->float('utility')->nullable()->default('0.00');
         });
     }
 
