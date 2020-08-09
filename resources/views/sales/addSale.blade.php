b91f0bdebc86a0d871590d6a3656d49953338124
Byron Jimenez <byronjimenez9911@gmail.com>
2020-08-08 19:56:17 -0600

    [Validation] Validate product items
    Also removeItem function has been init
    addSale view improved (Design)

--- resources/views/sales/addSale.blade.php
+++ resources/views/sales/addSale.blade.php
@@ -23,8 +23,8 @@
                         @include('sales.list_products')
                     </div>
                 </div>
-                <div class="col-md-4 p-0 h-100 overflow-auto" style="border-left: 1px solid #707070">
-                    <div class="content-header bg-white p-2" style="height: 10%">
+                <div style="border-left: 1px solid #707070" class="col-md-4 p-0  overflow-auto">
+                    <div class="content-header bg-white p-2" style="">
                         <div class="content-header row">
                             <div class="content-header-left col-md-4 h-100 my-auto">
                                 <h4>Detalles</h4>
@@ -32,78 +32,81 @@
                             <div class="content-header-right col-md-8">
                                 <div class="form-group">
                                     <input type="text" class="form-control" name="name" id="name"
-                                        placeholder="Nombre del cliente" autocomplete="off" />
+                                        placeholder="Nombre del cliente" autocomplete="off">
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="overflow-auto" style="height: 40%">
+                    <div class="overflow-auto" style="height: 35%">
                         <ul id="items" class="list-group py-1">
 
                         </ul>
                     </div>
-                    <!-- card-->
-                    <div class="card card-outline card-danger h-50">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="form-group col-md-3">
-                                    <label class="label-control">Descuentos</label>
-                                    <input type="decimal" class="form-control" id="additionalDiscounts"
-                                        name="additionalDiscounts" placeholder="Descuento adicional"
-                                        onkeyup="calculate()">
+                    <div class="position-absolute bottom-0">
+                        <!-- card-->
+                        <div class="card card-outline card-dangers mb-0">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="form-group col-md-3">
+                                        <label class="label-control">Descuentos</label>
+                                        <input type="decimal" class="form-control" id="additionalDiscounts"
+                                            name="additionalDiscounts" placeholder="Descuento adicional"
+                                            onkeyup="calculate()">
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label class="label-control">Pago adicional</label>
+                                        <input type="decimal" class="form-control" id="additionalPayments"
+                                            name="additionalPayments" placeholder="Pago adicional"
+                                            onkeyup="calculate()">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label class="label-control">Notas de la venta</label>
+                                        <textarea name="notes" id="notes" class="form-control"></textarea>
+                                    </div>
                                 </div>
-                                <div class="form-group col-md-3">
-                                    <label class="label-control">Pago adicional</label>
-                                    <input type="decimal" class="form-control" id="additionalPayments"
-                                        name="additionalPayments" placeholder="Pago adicional" onkeyup="calculate()">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label class="label-control">Notas de la venta</label>
-                                    <textarea name="notes" id="notes" class="form-control"></textarea>
-                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center py-05">
+                                        Cantidad total
+                                        <strong id="grandquantity">0</strong>
+                                        <input type="hidden" id="grandquantityvalue" name="grandquantityvalue">
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center py-05">
+                                        Sub total
+                                        <strong id="subtotal">$0.00</strong>
+                                        <input type="hidden" id="subtotalvalue" name="subtotalvalue">
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center py-05">
+                                        Descuentos
+                                        <strong id="discounts">$0.00</strong>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center py-05">
+                                        Pagos adicionales
+                                        <strong id="additionalpayments">$0.00</strong>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center py-05">
+                                        Impuestos
+                                        <strong id="tax">$0.00</strong>
+                                        <input type="hidden" id="taxvalue" name="taxesvalue">
+                                    </li>
+                                    <li class="list-group-item d-none justify-content-between align-items-center py-05"
+                                        id="grandinterest">
+                                        Interés
+                                        <strong id="interest">$0.00</strong>
+                                        <input type="hidden" id="interestvalue" name="interestvalue">
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center py-05">
+                                        Total
+                                        <strong id="grandtotal">$0.00</strong>
+                                        <input type="hidden" id="grandtotalvalue" name="grandtotalvalue">
+                                    </li>
+                                </ul>
+                                <input type="hidden" id="itemsCount" name="itemsCount" value="4"><!-- itemsCount-->
+                                <button type="submit" id="createSale" data-loading-text="Cargando..."
+                                    class="btn btn-success btn-block mt-2">Registrar factura</button>
                             </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item d-flex justify-content-between align-items-center py-05">
-                                    Cantidad total
-                                    <strong id="grandquantity">0</strong>
-                                    <input type="hidden" id="grandquantityvalue" name="grandquantityvalue">
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center py-05">
-                                    Sub total
-                                    <strong id="subtotal">$0.00</strong>
-                                    <input type="hidden" id="subtotalvalue" name="subtotalvalue">
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center py-05">
-                                    Descuentos
-                                    <strong id="discounts">$0.00</strong>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center py-05">
-                                    Pagos adicionales
-                                    <strong id="additionalpayments">$0.00</strong>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center py-05">
-                                    Impuestos
-                                    <strong id="tax">$0.00</strong>
-                                    <input type="hidden" id="taxvalue" name="taxesvalue">
-                                </li>
-                                <li class="list-group-item d-none justify-content-between align-items-center py-05"
-                                    id="grandinterest">
-                                    Interés
-                                    <strong id="interest">$0.00</strong>
-                                    <input type="hidden" id="interestvalue" name="interestvalue">
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center py-05">
-                                    Total
-                                    <strong id="grandtotal">$0.00</strong>
-                                    <input type="hidden" id="grandtotalvalue" name="grandtotalvalue">
-                                </li>
-                            </ul>
-                            <input type="hidden" id="itemsCount" name="itemsCount"><!-- itemsCount-->
-                            <button type="submit" id="createSale" data-loading-text="Cargando..."
-                                class="btn btn-success btn-block mt-2">Registrar factura</button>
                         </div>
+                        <!-- /.card -->
                     </div>
-                    <!-- /.card -->
                 </div>
             </div>
         </form>
