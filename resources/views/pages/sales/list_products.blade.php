e5dbcc3c756c4e3f45d822c123ead660ae63bc30
Byron Jimenez <byronjimenez9911@gmail.com>
2020-09-11 20:31:12 -0600

    Vue implementation

--- resources/views/pages/sales/list_products.blade.php
+++ resources/views/pages/sales/list_products.blade.php
@@ -1,7 +1,7 @@
 <div class="row py-2 px-1" id="products">
     @foreach ($products as $item)
     <div class="col-xl-2 col-lg-3 col-md-4">
-        <div class="card cursor-pointer" onclick="add({{ $item->id }})" style="height: 220px">
+        <div class="card cursor-pointer" v-on:click="add({{ $item->id }})" style="height: 220px">
             <div class="card-content h-100">
                 <div style="background-image: url('{{ asset($item->first_image->src) }}'); background-size: 
                     cover; height: 60%; background-position: center; background-repeat: no-repeat;">
