d6d4c8e8a7c7350b497ecf87e8a2528dd572f33e
Byron Jimenez <byronjimenez9911@gmail.com>
2020-09-17 19:53:38 -0600

    vuew integration to purcharse module

---
+++ resources/js/scripts/purchase/script.js
@@ -0,0 +1,17 @@
+import item from './components/Item.js'
+
+var vm = new Vue({
+    el : '#app',
+    data : {
+        items : [],
+        data : {
+            grandQuantity : 0.00,
+            subtotal : 0.00,
+            grandTotal : 0.00,
+            comments : "cxvdf"
+        },
+        newProduct : {
+
+        }
+    }
+})
