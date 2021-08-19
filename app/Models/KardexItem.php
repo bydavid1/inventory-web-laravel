0b0b58160511a929e653056a1d1400bbf6ecbb6e
bydavid1 <byronjimenez9911@gmail.com>
2021-08-18 18:43:48 -0600

    Kardex relations added

--- app/Models/KardexItem.php
+++ app/Models/KardexItem.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KardexItem extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    public function invoice() {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function kardexReport() {
+        return $this->belongsToMany(KardexReport::class);
+    }
 }
--- app/Models/KardexReport.php
+++ app/Models/KardexReport.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KardexReport extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    public function records() {
+        return $this->hasMany(KardexItem::class);
+    }
 }
