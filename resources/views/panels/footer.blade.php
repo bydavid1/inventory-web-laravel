faccba958de6accc4ae5ba27e9dd2d53c5dad884
bydavid1 <byronjimenez9911@gmail.com>
2021-08-30 08:44:46 -0600

    footer updated

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -1,9 +1,8 @@
 <footer class="footer footer-light @if(isset($configData['footerType'])){{$configData['footerClass']}}@endif">
   <p class="clearfix mb-0">
-    <span class="float-left d-inline-block">2020 &copy; PIXINVENT</span>
-    <span class="float-right d-sm-inline-block d-none">Crafted with
-      <i class="bx bxs-heart pink mx-50 font-small-3"></i>by
-      <a class="text-uppercase" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a>
+    <span class="float-left d-inline-block">2020 &copy; e-box</span>
+    <span class="float-right d-sm-inline-block d-none">Desarrollado por
+      <a class="text-uppercase" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Byron Jimenez</a>
     </span>
     @if($configData['isScrollTop'] === true)
     <button class="btn btn-primary btn-icon scroll-top" type="button">
